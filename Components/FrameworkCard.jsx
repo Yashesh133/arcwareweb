17ffd3870fa4c4a2cc3b0aec0eaa22d815490167
Yashesh133 <yashcreative133@gmail.com>
2024-04-27 10:51:16 +0530

    image

--- Components/FrameworkCard.jsx
+++ Components/FrameworkCard.jsx
@@ -5,7 +5,7 @@ const FrameworkCard = ({ image, name }) => {
   return (
     <div className='h-60 w-60 my-5 w-50 relative overflow-hidden'>
       <div className='bg-white p-5 m-5 rounded-[40px] transition-transform duration-300 hover:scale-110'>
-        <Image className='h-24 w-auto m-auto object-cover'
+        <Image className='h-24 w-24 object-cover'
           src={image}
           alt={name}
           height={100}
